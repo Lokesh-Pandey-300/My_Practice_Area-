251bad3a7b37135b65c802650736b0930c11fcc6
Lokesh-Pandey-300 <104666550+Lokesh-Pandey-300@users.noreply.github.com>
2024-05-04 02:16:31 +0530

    Codes

---
+++ FILES_C++/binary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //to print the reverse of an given number
+    int n;
+    cout<<"enter the number you want to reverse"<<endl;
+    cin>>n;
+    
+    return 0;
+}
